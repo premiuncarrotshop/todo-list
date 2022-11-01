7112d43b458901dce229511987b126d4b4c65dc3
premiuncarrotshop <benqu94@naver.com>
2022-11-01 19:45:05 +0900

    🚧 Add datetime attribute of .todolist-time and set its value

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const todolistTime = document.querySelector('.todolist-time');
 const date = todolistTime.querySelector('.date');
 const time = todolistTime.querySelector('.time');
 
+// Datetime
 const now = new Date();
 const nowYear = now.getFullYear();
 const nowMonth = now.getMonth() + 1;
@@ -9,6 +10,8 @@ const nowDate = now.getDate();
 
 const nowHours = now.getHours();
 const nowMinutes = now.getMinutes();
+
+todolistTime.setAttribute('datetime', `${nowYear}-${nowMonth}-${nowDate} ${nowHours}:${nowMinutes}`);
 
 date.innerText = `${nowYear}/${nowMonth}/${nowDate}`;
 time.innerText = `${nowHours}:${nowMinutes}`;
