71c31d5cebc7fcae5df83cd41ed9712c81193f99
premiuncarrotshop <pyo.seungyeonn@gmail.com>
2022-11-08 18:09:23 +0900

    🚧 Defined the showTodo and deleteTodo function.

--- js/todo.js
+++ js/todo.js
@@ -4,9 +4,31 @@ const todoList = document.querySelector('.todolist-list');
 
 todoForm.addEventListener('submit', handleTodoFormSubmit);
 
+function deleteTodo(e) {
+  const li = e.target.parentNode;
+  li.remove();
+}
+
+function showTodo(newTodo) {
+  const li = document.createElement('li');
+
+  const span = document.createElement('span');
+  span.innerText = newTodo;
+
+  const button = document.createElement('button');
+  button.innerText = '❎';
+  button.addEventListener('click', deleteTodo);
+
+  li.appendChild(span);
+  li.appendChild(button);
+  todoList.appendChild(li);
+}
+
 function handleTodoFormSubmit(e) {
   e.preventDefault();
 
   const newTodo = todoInput.value;
   todoInput.value = '';
+
+  showTodo(newTodo);
 }
